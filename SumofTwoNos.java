6e7cf5e3cec2665d1f38a2ba2d8334cfca4c9632
geetha0105 <geetha0105@gmail.com>
2020-01-31 09:25:38 -0500

    added a new file

---
+++ SumofTwoNos.java
@@ -0,0 +1,13 @@
+public class SumofTwoNos {
+    public static void main(String[] args) {
+        System.out.println();
+       int a;int b;int c;
+       a = 2;
+       b = 3;
+       c = a + b;
+
+        System.out.print("The sum is" + " " + c);
+
+
+    }
+}
