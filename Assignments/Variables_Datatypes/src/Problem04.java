1ce13e2e1c40fea3b17f3c8eca5b2eb32a08e773
geetha0105 <geetha0105@gmail.com>
2020-02-04 09:00:45 -0500

    Updates are done to files

--- Assignments/Variables_Datatypes/src/Problem04.java
+++ Assignments/Variables_Datatypes/src/Problem04.java
@@ -7,5 +7,26 @@ SUGGESTIONS:
 Use Math.PI for the pi value
 */
 
+import java.util.Scanner;
+
 public class Problem04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the radius of the circle");
+        float Radius = sc.nextFloat();
+
+//Calculating the Perimeter
+       float perimeter = (float) (2 * Math.PI * Radius);
+        System.out.println("The perimeter of the circle is " + perimeter);
+
+//Calculating the area
+       float area = (float)Math.PI * (float) (Math.pow (Radius, 2));
+        System.out.println("The area of the circle is " + area);
+
+
+
+
+    }
+
+
 }
